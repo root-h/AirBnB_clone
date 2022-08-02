26a3ddd50787d4a16a6d765d9f0e048c4a07e34a
Je4stine <je4stine@gmail.com>
2022-08-02 12:58:32 +0300

    Repo created

---
+++ console.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""The entry point for the command line interpreter"""
+import cmd, sys
+from turtle import *
+
---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This is the base model for the BNB project
+"""
+
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+    """
+    Defines all common attributes 
+    """
+
+    def __init__(self, *args, **kwargs):
+        pass
